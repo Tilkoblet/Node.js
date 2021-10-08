2484cf67a2185722ca4657bc21b40a904d485ac4
Seongwon Seo <seongwon.seo@tilkoblet.com>
2021-10-08 13:14:24 +0900

    국민연금EDI

---
+++ KR/OR/NPS/EDI/부과내역조회.js
@@ -0,0 +1,47 @@
+const FS = require("fs");
+const Rest = require("../../../../REST").Tilko.API.REST;
+const Constant = require("../../../Constant").Constant;
+
+var Tilko = Tilko || {};
+
+(function (_Tilko) {
+    
+    // API 상세설명 URL
+    // https://tilko.net/Help/Api/POST-api-apiVersion-NpsEdi-U040205M01
+    
+    try {
+        let _rest = new Rest(Constant.ApiKey);
+        _rest.Init();
+
+        // 국민연금EDI의 부과내역 조회 endPoint 설정
+        _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/npsedi/u040205m01");
+
+        // 공동인증서 경로 설정
+        const _publicPath = Constant.CertPath + "/signCert.der";
+        const _privatePath = Constant.CertPath + "/signPri.key";
+        let _publicCert = Buffer.alloc(0);
+        let _privateKey = Buffer.alloc(0);
+
+        _publicCert = FS.readFileSync(_publicPath);
+        _privateKey = FS.readFileSync(_privatePath);
+        console.log("_publicCert:", _publicCert);
+        console.log("_privateKey:", _privateKey);
+        
+        // Body 추가
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("BusinessNumber", "", true);                      // [암호화] 검색 할 사업자등록번호 또는 주민등록번호(xxxxxxxxxx 또는 xxxxxxxxxxxxx / Base64 인코딩)
+        _rest.AddBody("DocNo", "", false);                              // 문서 번호
+        _rest.AddBody("ConfirmDt", "", false);                          // 해당년월
+        _rest.AddBody("FmCd", "", false);                               // FmCd
+        _rest.AddBody("RgstChrgpId", "", false);                        // 사업장관리번호
+
+        // API 호출
+        const Response = _rest.Call();
+        console.log("Response:", Response);
+    } catch (e) {
+        console.error(e);
+    }
+
+})(Tilko);
