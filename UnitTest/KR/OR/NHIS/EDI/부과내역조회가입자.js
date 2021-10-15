d17cefe030c5720a430be1abea7e8616c66584fd
Seongwon Seo <seongwon.seo@tilkoblet.com>
2021-10-15 15:52:27 +0900

    UnitTest 샘플 코드 추가

---
+++ UnitTest/KR/OR/NHIS/EDI/부과내역조회가입자.js
@@ -0,0 +1,52 @@
+const FS = require("fs");
+const Rest = require("../../../../../Tilko.API/REST").Tilko.API.REST;
+const Constant = require("../../../../../UnitTest/Constant").UnitTest.Constant;
+
+var Tilko = Tilko || {};
+
+(function (_Tilko) {
+
+    // API 상세설명 URL
+    // https://tilko.net/Help/Api/POST-api-apiVersion-NhisEdi-BMBB_020
+    
+    try {
+        let _rest = new Rest(Constant.ApiKey);
+        _rest.Init();
+
+        // 건강보험공단EDI의 부과내역조회가입자 조회 endPoint 설정
+        _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/nhisedi/bmbb_020");
+
+        // 공동인증서 경로 설정
+        const _publicPath = Constant.CertPath + "/signCert.der";
+        const _privatePath = Constant.CertPath + "/signPri.key";
+        let _publicCert = Buffer.alloc(0);
+        let _privateKey = Buffer.alloc(0);
+
+        _publicCert = FS.readFileSync(_publicPath);
+        _privateKey = FS.readFileSync(_privatePath);
+        console.log("_publicCert:", _publicCert);
+        console.log("_privateKey:", _privateKey);
+        
+        // Body 추가
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("IdentityNumber", "", true);                      // [암호화] 검색 할 위임 사업자등록번호(xxxxxxxxxx / Base64 인코딩)
+        _rest.AddBody("FirmSym", "", false);                            // 사업장기호
+        _rest.AddBody("UnitFirmSym", "", false);                        // 단위사업장기호
+        _rest.AddBody("FirmMgmtNo", "", false);                         // 사업장관리번호
+        _rest.AddBody("WrtChasu", "", false);                           // 작성일차수
+        _rest.AddBody("WrtDupSeq", "", false);                          // 작성일차수 Seq
+        _rest.AddBody("DocId", "", false);                              // DocId
+        _rest.AddBody("GojiYyyymm", "", false);                         // 고지년월(yyyyMM)
+        _rest.AddBody("GojiChasu", "", false);                          // 고지차수
+        _rest.AddBody("NationFinanceCd", "", false);                    // NationFinanceCd
+        
+        // API 호출
+        const Response = _rest.Call();
+        console.log("Response:", Response);
+    } catch (e) {
+        console.error(e);
+    }
+
+})(Tilko);
