ebbc2646e904c4e8148c2e01b4be3347c85e7b4a
이유 <yu.lee@tilkoblet.com>
2021-10-20 18:54:12 +0900

    UnitTest 추가

---
+++ UnitTest/KR/GO/HOMETAX/원천세신고서조회-세무대리용.js
@@ -0,0 +1,47 @@
+const FS = require("fs");
+const Rest = require("../../../../Tilko.API/REST").Tilko.API.REST;
+const Constant = require("../../../../UnitTest/Constant").UnitTest.Constant;
+
+var Tilko = Tilko || {};
+
+(function (_Tilko) {
+
+    // API 상세설명 URL
+    // https://tilko.net/Help/Api/POST-api-apiVersion-HometaxAgent-UTERNAAZ110-WonCheonSe-SinGo
+    
+    try {
+        let _rest = new Rest(Constant.ApiKey);
+        _rest.Init();
+
+        // 홈택스의 원천세 신고서 조회 endPoint 설정
+        _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/hometaxagent/uternaaz110/woncheonse/singo");
+
+        // 공동인증서 경로 설정
+        const _publicPath = Constant.CertPath + "/signCert.der";
+        const _privatePath = Constant.CertPath + "/signPri.key";
+        let _publicCert = Buffer.alloc(0);
+        let _privateKey = Buffer.alloc(0);
+
+        _publicCert = FS.readFileSync(_publicPath);
+        _privateKey = FS.readFileSync(_privatePath);
+        console.log("_publicCert:", _publicCert);
+        console.log("_privateKey:", _privateKey);
+        
+        // Body 추가
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("AgentId", "", true);                             // [암호화] 세무대리인 ID(세무대리 관리번호가 있는 경우 / Base64 인코딩)
+        _rest.AddBody("AgentPassword", "", true);                       // [암호화] 세무대리인 암호(세무대리 관리번호가 있는 경우 / Base64 인코딩)
+        _rest.AddBody("BusinessNumber", "", true);                      // [암호화] 검색 할 사업자등록번호 또는 주민등록번호(xxxxxxxxxx 또는 xxxxxxxxxxxxx / Base64 인코딩) 공백일 검색기간은 30일, 아닐경우 검색기간은 365일
+        _rest.AddBody("StartDate", "2020", false);                      // 검색시작일(yyyyMMdd) 공백일 경우 기본값을 API에서 셋팅
+        _rest.AddBody("EndDate", "2020", false);                        // 검색종료일(yyyyMMdd) 공백일 경우 기본값을 API에서 셋팅
+
+        // API 호출
+        const Response = _rest.Call();
+        console.log("Response:", Response);
+    } catch (e) {
+        console.error(e);
+    }
+
+})(Tilko);
---
+++ UnitTest/KR/GO/HOMETAX/전자세금계산서합계표조회-세무대리용.js
@@ -0,0 +1,48 @@
+const FS = require("fs");
+const Rest = require("../../../../Tilko.API/REST").Tilko.API.REST;
+const Constant = require("../../../../UnitTest/Constant").UnitTest.Constant;
+
+var Tilko = Tilko || {};
+
+(function (_Tilko) {
+
+    // API 상세설명 URL
+    // https://tilko.net/Help/Api/POST-api-apiVersion-HometaxAgent-UTEETBDA17-SuImNabSeJa-JeonJaSeGeumGyeSanSeo-HabGyePyo
+    
+    try {
+        let _rest = new Rest(Constant.ApiKey);
+        _rest.Init();
+
+        // 홈택스의 전자(세금)계산서 합계표조회 endPoint 설정
+        _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/hometaxagent/uteetbda17/suimnabseja/jeonjasegeumgyesanseo/habgyepyo");
+
+        // 공동인증서 경로 설정
+        const _publicPath = Constant.CertPath + "/signCert.der";
+        const _privatePath = Constant.CertPath + "/signPri.key";
+        let _publicCert = Buffer.alloc(0);
+        let _privateKey = Buffer.alloc(0);
+
+        _publicCert = FS.readFileSync(_publicPath);
+        _privateKey = FS.readFileSync(_privatePath);
+        console.log("_publicCert:", _publicCert);
+        console.log("_privateKey:", _privateKey);
+        
+        // Body 추가
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("AgentId", "", true);                             // [암호화] 세무대리인 ID(세무대리 관리번호가 있는 경우 / Base64 인코딩)
+        _rest.AddBody("AgentPassword", "", true);                       // [암호화] 세무대리인 암호(세무대리 관리번호가 있는 경우 / Base64 인코딩)
+        _rest.AddBody("BusinessNumber", "", true);                      // [암호화] 검색 할 사업자등록번호 또는 주민등록번호(xxxxxxxxxx 또는 xxxxxxxxxxxxx / Base64 인코딩)
+        _rest.AddBody("Year", "", false);                               // 검색년도(yyyy) 공백일 경우 검색 기준 해
+        _rest.AddBody("StartMonth", "", false);                         // 검색 시작 월(MM) 공백일 경우 검색 기준 월
+        _rest.AddBody("EndMonth", "", false);                           // 검색 종료 월(MM) 공백일 경우 검색 기준 월
+
+        // API 호출
+        const Response = _rest.Call();
+        console.log("Response:", Response);
+    } catch (e) {
+        console.error(e);
+    }
+
+})(Tilko);
