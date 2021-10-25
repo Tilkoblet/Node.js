7fd71cd4992e48e80ad3752e1f4f450ab61a9f90
이유 <yu.lee@tilkoblet.com>
2021-10-25 18:56:50 +0900

    .

--- UnitTest/KR/OR/NHIS/건강보험자격득실내역-인증서로그인용.js
+++ UnitTest/KR/OR/NHIS/건강보험자격득실내역-인증서로그인용.js
@@ -1,6 +1,6 @@
 const FS = require("fs");
-const Rest = require("../../../../Tilko.API/REST").Tilko.API.REST;
-const Constant = require("../../../../UnitTest/Constant").UnitTest.Constant;
+const Rest = require("../../../../../Tilko.API/REST").Tilko.API.REST;
+const Constant = require("../../../../../UnitTest/Constant").UnitTest.Constant;
 
 var Tilko = Tilko || {};
 
@@ -15,7 +15,7 @@ var Tilko = Tilko || {};
 
         // 국민건강보험공단의 건강보험자격득실내역 endPoint 설정
         _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/nhis/jpaea00401");
-
+        
         // 공동인증서 경로 설정
         const _publicPath = Constant.CertPath + "/signCert.der";
         const _privatePath = Constant.CertPath + "/signPri.key";
@@ -26,18 +26,13 @@ var Tilko = Tilko || {};
         _privateKey = FS.readFileSync(_privatePath);
         console.log("_publicCert:", _publicCert);
         console.log("_privateKey:", _privateKey);
-        
+
         // Body 추가
-        _rest.AddBody("NhisQuery", "", false);                   // 검색조건 전체 : 0 / 직장가입자 : 1 / 지역가입자 : 2 / 가입자 전체 : 3
-        _rest.AddBody("CxId", "", false);                        // CxId
-        _rest.AddBody("PrivateAuthType", "", false);             // 인증종류 KakaoTalk / Payco / KbMobile / SamsungPass / TelecomPass
-        _rest.AddBody("ReqTxId", "", false);                     // ReqTxId
-        _rest.AddBody("Token", "", false);                       // Token
-        _rest.AddBody("TxId", "", false);                        // TxId      
-        _rest.AddBody("UserName", "", true);                     // [암호화] 이용자명
-        _rest.AddBody("BirthDate", "", true);                    // [암호화] 생년월일(yyyyMMdd)
-        _rest.AddBody("UserCellphoneNumber", "", true);          // [암호화] 휴대폰번호
-        
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("NhisQuery", "", false);                          // 검색조건 전체 : 0 / 직장가입자 : 1 / 지역가입자 : 2 / 가입자 전체 : 3
+
         // API 호출
         const Response = _rest.Call();
         console.log("Response:", Response);
