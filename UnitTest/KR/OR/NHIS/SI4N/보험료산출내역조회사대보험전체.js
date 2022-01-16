78228da9fe3684471145b8cfd64fa8c1786afd5f
Seongwon Seo <seongwon.seo@tilkoblet.com>
2022-01-17 07:58:26 +0900

    .

---
+++ UnitTest/KR/OR/NHIS/SI4N/보험료산출내역조회사대보험전체.js
@@ -0,0 +1,45 @@
+const FS = require("fs");
+const Rest = require("../../../../../Tilko.API/REST").Tilko.API.REST;
+const Constant = require("../../../../../UnitTest/Constant").UnitTest.Constant;
+
+var Tilko = Tilko || {};
+
+(function (_Tilko) {
+
+    // API 상세설명 URL
+    // https://tilko.net/Help/Api/POST-api-apiVersion-NhisSi4n-JpBca00205
+    
+    try {
+        let _rest = new Rest(Constant.ApiKey);
+        _rest.Init();
+
+        // 국민건강보험공단의 보험료 산출내역 조회(4대보험 전체) endPoint 설정
+        _rest.SetEndPointUrl(Constant.ApiHost + "api/v1.0/nhissi4n/JpBca00205");
+
+        // 공동인증서 경로 설정
+        const _publicPath = Constant.CertPath + "/signCert.der";
+        const _privatePath = Constant.CertPath + "/signPri.key";
+        let _publicCert = Buffer.alloc(0);
+        let _privateKey = Buffer.alloc(0);
+
+        _publicCert = FS.readFileSync(_publicPath);
+        _privateKey = FS.readFileSync(_privatePath);
+        console.log("_publicCert:", _publicCert);
+        console.log("_privateKey:", _privateKey);
+        
+        // Body 추가
+        _rest.AddBody("CertFile", _publicCert, true);                   // [암호화] 인증서 공개키(Base64 인코딩)
+        _rest.AddBody("KeyFile", _privateKey, true);                    // [암호화] 인증서 개인키(Base64 인코딩)
+        _rest.AddBody("CertPassword", Constant.CertPassword, true);     // [암호화] 인증서 암호(Base64 인코딩)
+        _rest.AddBody("BusinessNumber", "", true);                      // [암호화] 사업자등록번호(Base64 인코딩)
+        _rest.AddBody("Year", "2021", false);                           // 고지년도(yyyy)
+        _rest.AddBody("Month", "12", false);                            // 고지월(MM)
+        
+        // API 호출
+        const Response = _rest.Call();
+        console.log("Response:", JSON.stringify(Response));
+    } catch (e) {
+        console.error(e);
+    }
+
+})(Tilko);
